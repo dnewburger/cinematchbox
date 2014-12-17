549cf1b7aa829be3d3052bf6303f18d87154d1d3
dnewburger <den7@stanford.edu>
2014-12-17 15:19:12 -0800

    Added new index page plus modified database schema

--- cinematchbox.py
+++ cinematchbox.py
@@ -50,47 +50,46 @@ def close_db_connection(exception):
 
 
 @app.route('/')
-def show_entries():
-    return render_template('bootstrap_navbar_fixed_top_base.html')
-    # db = get_db()
-    # cur = db.execute('select title, text from entries order by id desc')
-    # entries = cur.fetchall()
-    # return render_template('show_entries.html', entries=entries)
+def show_home():
+    db = get_db()
+    cur = db.execute('select title, description from movies order by id desc')
+    movies = cur.fetchall()
+    return render_template('index.html', movies=movies)
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
+        print 'not logged in'
         abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    db.execute('insert into movies (title, description) values (?, ?)',
+                 [request.form['title'], request.form['description']])
     db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    flash('New movie was successfully saved')
+    return redirect(url_for('show_home'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # error = None
-    # if request.method == 'POST':
-    #     if request.form['username'] != app.config['USERNAME']:
-    #         error = 'Invalid username'
-    #     elif request.form['password'] != app.config['PASSWORD']:
-    #         error = 'Invalid password'
-    #     else:
-    #         session['logged_in'] = True
-    #         flash('You were logged in')
-    #         return redirect(url_for('show_entries'))
-    # return render_template('login.html', error=error)
-    return render_template('bootstrap_signin.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_home'))
+    return render_template('bootstrap_signin.html', error=error)
 
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_home'))
 
 
 if __name__ == '__main__':
--- schema.sql
+++ schema.sql
@@ -1,6 +1,30 @@
-drop table if exists entries;
-create table entries (
+drop table if exists users;
+create table users (
+  id integer primary key autoincrement,
+  name text not null,
+  email text not null,
+  password text not null
+);
+
+drop table if exists movies;
+create table movies (
   id integer primary key autoincrement,
   title text not null,
-  text text not null
+  description text not null
+);
+
+drop table if exists ratings;
+create table ratings (
+	id integer primary key autoincrement,
+	userid integer,
+	movieid integer,
+	rating integer default 0,
+	has_whatched boolean,
+	watch_again_rating integer
+);
+
+drop table if exists friends;
+create table friends (
+	user_id_a integer,
+	user_id_b integer
 );
